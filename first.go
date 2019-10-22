5d19c77da791e8c71842a33cf98b9b9749869f96
Toytum <steath1999@gmail.com>
2019-10-22 13:43:29 -0700

    Initial commit

---
+++ first.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Welcome to the Solar System!")
+	fmt.Println("There are 9 planets to explore. Yes, Pluto is a planet, get over yourself")
+	fmt.Println("What is your name?")
+	var name string = "Ethan"
+	fmt.Println("Nice to meet you " + name + ", My name is Alexa, and I spy on your conversations")
+	fmt.Println("Let's go out and have some fun shall we?")
+	fmt.Println("What planet would you like to explore?")
+
+	fmt.Println()
+
+}
